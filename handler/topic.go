126a3b186fd0af6cfb05efed3a88e92e46081788
mhafidk <mhafidk@gmail.com>
2023-08-26 12:56:36 +0700

    add update topic endpoint

--- handler/topic.go
+++ handler/topic.go
@@ -7,6 +7,11 @@ import (
 	"github.com/mhafidk/ngartos/model"
 )
 
+type updateTopic struct {
+	Name string `json:"name"`
+	Content string `json:"content"`
+}
+
 func CreateTopic(c *fiber.Ctx) error {
 	db := database.DB.Db
 	topic := new(model.Topic)
@@ -55,6 +60,54 @@ func GetSingleTopic(c *fiber.Ctx) error {
 	return c.Status(200).JSON(fiber.Map{
 		"status": "success",
 		"message": "User found",
+		"data": fiber.Map{
+			"name": topic.Name,
+			"content": topic.Content,
+			"createdAt": topic.CreatedAt,
+			"updatedAt": topic.UpdatedAt,
+		},
+	})
+}
+
+func UpdateTopic(c *fiber.Ctx) error {
+	db := database.DB.Db
+
+	var topic model.Topic
+
+	id := c.Params("id")
+
+	db.Find(&topic, "id = ?", id)
+	if topic.ID == uuid.Nil {
+		return c.Status(404).JSON(fiber.Map{
+			"status": "not found",
+			"message": "Topic not found",
+			"data": nil,
+		})
+	}
+
+	var updateTopicData updateTopic
+	err := c.BodyParser(&updateTopicData)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"status": "error",
+			"message": "Something is wrong with the input data",
+			"data": err,
+		})
+	}
+
+	topic.Name = updateTopicData.Name
+	err = db.Save(&topic).Error
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"status": "error",
+			"message": "Could not update the topic",
+			"data": err,
+		})
+	}
+
+	return c.Status(200).JSON(fiber.Map{
+		"status": "success",
+		"message": "User updated",
 		"data": fiber.Map{
 			"name": topic.Name,
 			"content": topic.Content,
--- router/router.go
+++ router/router.go
@@ -29,4 +29,5 @@ func SetupRoutes(app *fiber.App) {
 
 	topics.Post("/", handler.CreateTopic)
 	topics.Get("/:id", handler.GetSingleTopic)
+	topics.Put("/:id", handler.UpdateTopic)
 }
